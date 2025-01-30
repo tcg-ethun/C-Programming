48dc997025122fe581a5acd1c50a82ab36794ad8
SPY-ESAN <github.ethun@gmail.com>
2025-01-29 20:13:05 -0800

    Add C Programming Files

---
+++ Minus.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+ int a,b,minus;
+ scanf("%d",&a);
+ scanf("%d",&b);
+ minus = a-b;
+ printf("Result of Minus: %d",minus);
+return 0;
+}
---
+++ Sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+ int a,b,Sum;
+ scanf("%d",&a);
+ scanf("%d",&b);
+ Sum = a+b;
+ printf("Result of Sum: %d",Sum);
+return 0;
+}
