4ddd7b076544a7cbb67194e5a333c0e2e17901df
tcgethun <ethuntcg@gmail.com>
2025-02-26 21:37:43 +0600

    Update

--- Practice/35_Practice.c
+++ Practice/35_Practice.c
@@ -1,8 +1,15 @@
- 
-#include<stdio.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-   int main(){
-   
- 
-   return 0;
+int main() {
+    srand(time(0));  // Seed with current time
+
+    int otp = 1000 + rand() % 9000;  // 4-digit OTP
+    printf("4-digit OTP: %d\n", otp);
+
+    int otp6 = 100000 + rand() % 900000;  // 6-digit OTP
+    printf("6-digit OTP: %d\n", otp6);
+
+    return 0;
 }
