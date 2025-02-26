eed28d7f7fd22d6fb79cad72055e81341307471d
tcgethun <ethuntcg@gmail.com>
2025-02-26 20:41:43 +0600

    Update

---
+++ Practice/34_Practice.c
@@ -0,0 +1,38 @@
+ 
+#include<stdio.h> 
+
+void unlock();
+void tryAgain();
+
+   int main(){
+   int input,i,password;
+   printf("Enter Password : ");
+   scanf("%d",&password);
+   printf("Match the password : ");
+   scanf("%d",&input);
+   if(input==password){
+    unlock();
+    return 0;
+   }
+
+else{
+  for(i=0;  ;i++){
+   if(input==password){
+    unlock();
+    return 0;
+   } 
+   tryAgain();
+   printf("Enter the password Again : ");
+   scanf("%d",&input);
+  }
+}
+   return 0;
+}
+
+void unlock(){
+ printf("Congratulations ! Password Matched . \n");
+}
+void tryAgain(){
+ printf("Please try again :(  \n");
+
+}
