59ee08e9125378a164f15d4e01d0b802164d3cba
tcgethun <ethuntcg@gmail.com>
2025-02-27 10:43:53 +0600

    Update

--- Practice/38_Practice.c
+++ Practice/38_Practice.c
@@ -2,16 +2,17 @@
 #include<stdio.h> 
 
 
-void countdown(int n){
-      if(n == 11){
-        return;
-      }
-      printf("%d \n",n);
-      countdown(n+1);
+void countdown(int n);
+
+    int main(){
+  countdown(1);
+    return 0;
 }
 
-
-   int main(){
-         countdown(1);
-   return 0;
+void countdown(int n){
+   if(n==10){
+     return;
+   }
+   printf("%d \n",n);
+   countdown(n+1);
 }
