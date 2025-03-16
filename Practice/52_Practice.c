6421b0cbe0199f2061d0ca6faf1a8b58192ca35f
Esan <ethuntcg@gmail.com>
2025-03-16 12:41:38 +0600

    Update

--- Practice/52_Practice.c
+++ Practice/52_Practice.c
@@ -2,7 +2,11 @@
 #include<stdio.h> 
 
    int main(){
-   
- 
+   int arr [] = {20,40,23,27};
+   int size = sizeof(arr) / sizeof(arr[0]);
+   printf("Array is : ");
+   for(int i=0; i < size;i++){
+      printf("%d ",arr[i]);
+   }
    return 0;
 }
